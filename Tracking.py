8fae782442526be99530ea41d20bf59c30eefc38
Logan Luna <LoganLambeth8@gmail.com>
2024-04-03 17:39:36 -0400

    Some visualization of data

--- Tracking.py
+++ Tracking.py
@@ -22,7 +22,7 @@ last_known_keypoints = {}
 
 # Directory containing the image frames
 #frames_directory = "Jet2/img"
-frames_directory = "Jet1/img"
+frames_directory = "Jet2/img"
 
 # Get list of image files in sorted order
 frame_files = sorted([f for f in os.listdir(frames_directory) if f.endswith('.jpg')])
